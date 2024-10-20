3486fc50defaf0226cdff2f295bc5db4e0d8365f
Goob-tech <67513182+Goob-hub@users.noreply.github.com>
2024-10-20 12:53:37 -0500

    using npm
    used npm to install simple packages to generate random names

---
+++ Node JS/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+// var generateName = require("sillyname");
+
+import superHero from 'superheroes';
+// import generateName from "sillyname";
+
+// let sillyName = generateName();
+let superHeroName = superHero.random();
+
+// console.log(`My name is ${sillyName}.`);
+console.log(`I am ${superHeroName}!!`);
