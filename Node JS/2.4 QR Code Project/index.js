e22775fc5135744fb75360d57cd681341b49eb5b
Goob-tech <67513182+Goob-hub@users.noreply.github.com>
2024-10-22 14:58:02 -0500

    Used npm packages to create qr code creator
    Used inquirer.js and qr-image npm packages to create a small program that prompts the user in the terminal to input a url
    
    that input is then taken and a file is created with that input as well as a png file that contains the qr code for said input

--- Node JS/2.4 QR Code Project/index.js
+++ Node JS/2.4 QR Code Project/index.js
@@ -3,3 +3,39 @@
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+
+import fs from "fs";
+import inquirer from "inquirer";
+import qr from "qr-image";
+
+inquirer.prompt([
+    {
+        type: "input",
+        name: "url",
+        message: "Give me your URL so I can turn it into a QR code image!"
+    }
+  ])
+  .then((answers) => {
+    createFileAndQR(answers);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+      console.log("Promt cant be loaded.")
+    } else {
+      // Something else went wrong
+      console.log("IDK what the F U C K went wrong.", error);
+    }
+});
+
+function createFileAndQR(data) {
+    let url = data.url;
+    fs.writeFile('QrURl.txt', url, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+
+    const qr_png = qr.image(url, { type: 'png' });
+
+    qr_png.pipe(fs.createWriteStream('yourQRCode.png'));
+}
