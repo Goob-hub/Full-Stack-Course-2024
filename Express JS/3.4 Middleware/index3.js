4ccb864d0e22de125ef7d1115b29bf5dd0cc9644
Goob-tech <67513182+Goob-hub@users.noreply.github.com>
2024-10-24 15:09:20 -0500

    handling form submissions
    Using middleware to handle requests and responses
    
    used bodyparser and morgan middlewares

--- Express JS/3.4 Middleware/index3.js
+++ Express JS/3.4 Middleware/index3.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.use(logger);
+app.use(myMiddleWare);
 
 app.get("/", (req, res) => {
   res.send("Hello");
@@ -12,3 +12,9 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+function myMiddleWare(req, res, next){
+  console.log("request method: ", req.method);
+  console.log("request url: ", req.url);
+  next();
+}
--- Express JS/3.4 Middleware/index4.js
+++ Express JS/3.4 Middleware/index4.js
@@ -1,7 +1,24 @@
 import express from "express";
+import bodyParser from "body-parser"
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.send(`<h1> ${req.body.street}${req.body.pet} is your band name! </h1>`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
