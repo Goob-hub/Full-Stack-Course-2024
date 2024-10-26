3a85ad4be45621a26670be64ca882be49f6e6334
Goob-tech <67513182+Goob-hub@users.noreply.github.com>
2024-10-26 10:11:29 -0500

    Created script for password checking
    Created a small script for a project that checks for a form input/submission
    
    The user must enter the correct password to see the next page otherwise they will get redirected to the home page
    
    If you are reading this for some reason then congrats lol

--- Express JS/3.5 Secrets Project/index.js
+++ Express JS/3.5 Secrets Project/index.js
@@ -1,3 +1,34 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+import bodyParser from "body-parser";
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const password = "TigBitties";
+const app = express();
+const port = 3000;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+    let userInput = req.body.password;
+
+    if(userInput === password) res.sendFile(__dirname + "/public/secret.html");
+
+    if(userInput !== password) res.redirect("/");
+});
+
+app.listen(port, (req, res) => {
+        console.log(`Server now running on port:${port}`);
+});
