6a3ece8d0b3384f5a9a80d0c08334fa3627bde9a
Goob-tech <67513182+Goob-hub@users.noreply.github.com>
2024-11-08 11:28:12 -0600

    Reading data from database
    Getting the app to read from the database and use said data

--- PostgreSQL/8.3 Travel Tracker/index.js
+++ PostgreSQL/8.3 Travel Tracker/index.js
@@ -1,5 +1,17 @@
+import 'dotenv/config';
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT),
+});
+
+db.connect();
 
 const app = express();
 const port = 3000;
@@ -7,8 +19,28 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let visitedCountries = [];
+
+let data = {
+  total: 0,
+  countries: visitedCountries
+}
+
+db.query("SELECT * FROM visited_countries", (err, res) => {
+  if(err) {
+    console.error(err.stack);
+  } else {
+    res.rows.forEach(row => {
+      visitedCountries.push(row.country_code);
+      data.total++;
+    });
+  }
+});
+
+
 app.get("/", async (req, res) => {
-  //Write your code here.
+  console.log(visitedCountries);
+  res.render("index.ejs", data);
 });
 
 app.listen(port, () => {
