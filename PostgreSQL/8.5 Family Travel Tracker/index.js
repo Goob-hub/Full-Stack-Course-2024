143240f191515a54eb04bf3d0fbf16b893afff5e
Goob-tech <67513182+Goob-hub@users.noreply.github.com>
2024-11-10 16:49:43 -0600

    Can add users to database and fetch them
    You can now add users to the database and every user in said database will be correctly displayed every time the home page is rendered.

--- PostgreSQL/8.5 Family Travel Tracker/index.js
+++ PostgreSQL/8.5 Family Travel Tracker/index.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
@@ -6,23 +7,19 @@ const app = express();
 const port = 3000;
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "world",
-  password: "123456",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT),
 });
+
 db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let currentUserId = 1;
-
-let users = [
-  { id: 1, name: "Angela", color: "teal" },
-  { id: 2, name: "Jack", color: "powderblue" },
-];
 
 async function checkVisisted() {
   const result = await db.query("SELECT country_code FROM visited_countries");
@@ -32,8 +29,19 @@ async function checkVisisted() {
   });
   return countries;
 }
+
+async function getUsers() {
+  const result = await db.query("SELECT * FROM users");
+  let users = [];
+  result.rows.forEach((user) => {
+    users.push(user);
+  });
+  return users;
+}
+
 app.get("/", async (req, res) => {
   const countries = await checkVisisted();
+  const users = await getUsers();
   res.render("index.ejs", {
     countries: countries,
     total: countries.length,
@@ -41,6 +49,7 @@ app.get("/", async (req, res) => {
     color: "teal",
   });
 });
+
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
 
@@ -65,11 +74,37 @@ app.post("/add", async (req, res) => {
     console.log(err);
   }
 });
-app.post("/user", async (req, res) => {});
+
+app.post("/user", async (req, res) => {
+  const input = req.body;
+  let user = input.user;
+
+  if(input.add) {
+    res.render("new.ejs");
+  } else {
+    try {
+      // Grab data from database for specific user
+    } catch (error) {
+      // Handle errors
+    }
+  }
+});
 
 app.post("/new", async (req, res) => {
   //Hint: The RETURNING keyword can return the data that was inserted.
   //https://www.postgresql.org/docs/current/dml-returning.html
+  const user = req.body;
+  try {
+    if(!user.name || !user.color) {
+      throw new Error("Missing data for user, could not add to database!");
+    }
+    const result = db.query(`INSERT INTO users (color, name) VALUES ('${user.color}', '${user.name}')`);
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+
+  res.redirect("/");
 });
 
 app.listen(port, () => {
