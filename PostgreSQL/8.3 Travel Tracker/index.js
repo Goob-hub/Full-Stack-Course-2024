28e88ae03efb4257aae0fbccaf068809eb9bd596
Goob-tech <67513182+Goob-hub@users.noreply.github.com>
2024-11-09 20:00:20 -0600

    Error handling and QOL
    Made it so that the user gets feedback if they enter an improper country name or something goes wrong
    
    Also added a QOL feature where the user can enter only part of the country name in lower or uppercase and they'll still get the correct country.

--- PostgreSQL/8.3 Travel Tracker/index.js
+++ PostgreSQL/8.3 Travel Tracker/index.js
@@ -19,12 +19,14 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let data;
+let data = {};
+let err;
 
 app.get("/", async (req, res) => {
   data = {
     total: 0,
-    countries: []
+    countries: [],
+    error: err
   };
 
   try { 
@@ -35,8 +37,10 @@ app.get("/", async (req, res) => {
       data.total++;
     });
   } catch (error) {
+    data.error = "Problem with getting visited countries!"
     console.error(error.message);
   }
+
   res.render("index.ejs", data);
   
 });
@@ -44,9 +48,15 @@ app.get("/", async (req, res) => {
 app.post("/add", async (req, res) => {
   let countryName = req.body.country;
   let isDuplicateCountry = false;
+  err = null;
 
   try { 
-    let response = await db.query(`SELECT * FROM countries WHERE country_name = '${countryName}'`);
+    if(!countryName) {
+      throw new Error("No input data provided");
+    }
+
+    let capitalizedInput = countryName.charAt(0).toUpperCase() + countryName.slice(1).toLowerCase();
+    let response = await db.query(`SELECT * FROM countries WHERE country_name LIKE '%' || '${capitalizedInput}' || '%'`);
     let dbData = response.rows[0];
 
     data.countries.forEach(countryCode => {
@@ -60,6 +70,7 @@ app.post("/add", async (req, res) => {
     }
 
   } catch (error) {
+    err = "Country does not exist, try again!";
     console.error(error.message);
   }
 
