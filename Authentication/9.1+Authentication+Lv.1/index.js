b086455a503e680ee7d9ff4bc4f22a0da0bec730
Goob-tech <67513182+Goob-hub@users.noreply.github.com>
2024-12-09 16:53:49 -0600

    Created login authentication
    This code can register and verify users by their email and password to view a secret page

--- Authentication/9.1+Authentication+Lv.1/index.js
+++ Authentication/9.1+Authentication+Lv.1/index.js
@@ -1,11 +1,42 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import 'dotenv/config';
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT),
+});
+
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+async function createUser(email, password) {
+  try {
+    const response = await db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [email, password]);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function verifyUser(email, password) {
+  const response = await db.query(`SELECT * FROM users WHERE email = $1 AND password = $2`, [email, password]);
+  const user = response.rows[0];
+  
+  if(user.email === email && user.password === password) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
@@ -19,9 +50,37 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
 
-app.post("/login", async (req, res) => {});
+  try {
+    const response = await createUser(email, password);
+    res.redirect("/");
+  } catch (error) {
+    console.error(error);
+    res.redirect("/register");
+  }
+
+});
+
+app.post("/login", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+
+  try {
+    const response = await verifyUser(email, password);
+    if(response) {
+      res.render("secrets.ejs");
+    } else {
+      res.redirect("/login");
+    }
+  } catch (error) {
+    console.error(error);
+    res.redirect("/");
+  }
+
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
