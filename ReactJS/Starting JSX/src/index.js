f5efa715ebd309cd783c2f80e22689271b46cc2a
Goob-tech <solizg02@gmail.com>
2024-12-20 13:02:45 -0600

    Starting react stuff

---
+++ ReactJS/Starting JSX/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Wus gud bby mama.</h1>
+    <p>I like bbq.</p>
+  </div>,
+  document.getElementById("root")
+);
