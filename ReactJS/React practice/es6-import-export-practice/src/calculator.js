fb15e9477053d935042cbba1f43345f3ea15ba11
Goob-tech <solizg02@gmail.com>
2025-01-04 10:23:38 -0600

    Import/export practice
    yummers

---
+++ ReactJS/React practice/es6-import-export-practice/src/calculator.js
@@ -0,0 +1,34 @@
+// const calculator = {
+//   add: function (n1, n2) {
+//     return n1 + n2;
+//   },
+//   multiply: function (n1, n2) {
+//     return n1 * n2;
+//   },
+//   subtract: function (n1, n2) {
+//     return n1 - n2;
+//   },
+//   divide: function (n1, n2) {
+//     return n1 / n2;
+//   },
+// };
+
+function add(n1, n2) {
+  return n1 + n2;
+}
+
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function subtract(n1, n2) {
+  return n1 - n2;
+}
+
+function divide(n1, n2) {
+  return n1 / n2;
+}
+
+// export default calculator;
+
+export { add, multiply, subtract, divide };
