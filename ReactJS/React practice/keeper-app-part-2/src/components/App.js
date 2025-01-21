b15af5e580366f3fb6fc16cd7a0d5ed763e045f9
Goob-tech <solizg02@gmail.com>
2025-01-21 13:14:23 -0600

    another small project with mapping
    yummers

---
+++ ReactJS/React practice/keeper-app-part-2/src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function createNote(props) {
+  return <Note key={props.key} title={props.title} content={props.content} />;
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(createNote)}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
